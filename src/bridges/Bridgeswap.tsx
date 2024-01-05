5202a4b168f23b232ca222f46f989d7b76416008
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2024-01-05 01:45:02 +0100

    Update Bridgeswap.tsx

--- src/bridges/Bridgeswap.tsx
+++ src/bridges/Bridgeswap.tsx
@@ -7,6 +7,7 @@ const widgetConfig: WidgetConfig = {
   containerStyle: {
     border: '1px solid rgb(234, 234, 234)',
     borderRadius: '16px',
+    logoURI:'https://postimg.cc/gallery/tbHPmcH'
   },
 };
 

a91263af7acec6a12dbfc79ca3da6f10eb7ed10b
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2025-07-24 13:36:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ethers } from 'ethers';
-
-// Esponi ethers globalmente per compatibilità
-window.ethers = ethers;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
